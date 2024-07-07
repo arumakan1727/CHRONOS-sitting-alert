155c50f6655d1dfb9df1a45724746a32f963d272
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2024-07-07 14:31:53 +0900

    add wifi utils and util types

---
+++ arduino/include/armkn/types.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include <Arduino.h>
+
+namespace armkn {
+
+using MilliTimeUInt = decltype(millis());
+static_assert(std::is_unsigned_v<MilliTimeUInt>);
+
+using Task = void (*)();
+
+class RepeatCount {
+ public:
+  using repeat_t = unsigned int;
+
+  static const RepeatCount INF;
+
+  static constexpr RepeatCount of(repeat_t count) { return RepeatCount(count); }
+
+  inline constexpr bool is_infinite() const { return *this == INF; }
+
+  inline constexpr operator repeat_t() const { return count; }
+
+  inline constexpr bool operator==(const RepeatCount &other) const {
+    return count == other.count;
+  }
+
+  inline constexpr bool operator!=(const RepeatCount &other) const {
+    return !(*this == other);
+  }
+
+ private:
+  RepeatCount() = default;
+  explicit constexpr RepeatCount(repeat_t count) : count(count) {}
+
+  repeat_t count;
+};
+
+inline const RepeatCount RepeatCount::INF = RepeatCount(-1);
+
+}  // namespace armkn
---
+++ arduino/include/armkn/utils/wifi.hpp
@@ -0,0 +1,63 @@
+#include <Arduino.h>
+#include <WiFiS3.h>
+
+#include <armkn/types.hpp>
+
+namespace armkn {
+
+void print_wifi_status() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("Local IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+void setup_wifi(
+  const char *ssid,
+  const char *password,
+  const Task on_success,
+  const Task on_failure
+) {
+  // Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ;  // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    on_failure();
+    // don't continue
+    while (true)
+      ;
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to WiFi network:
+  for (int status = WL_IDLE_STATUS; status != WL_CONNECTED; delay(5000)) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP
+    // network:
+    status = WiFi.begin(ssid, password);
+  }
+
+  on_success();
+}
+
+}  // namespace armkn
